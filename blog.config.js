c985810aaba2d40760631fd049b2f7b092642219
干殳 <86753671+guili918@users.noreply.github.com>
2021-07-01 17:50:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  socialLink: 'https://m.okjike.com/users/22036bdb-5fe7-4540-ac89-565b81ad9ffe?ref=PROFILE_CARD&utm_source=user_card',
+  socialLink: 'https://web.okjike.com/u/22036bdb-5fe7-4540-ac89-565b81ad9ffe',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
