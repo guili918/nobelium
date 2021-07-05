6e42bd81d1ef2fba3e4a71605be94e7535b915d2
干殳 <86753671+guili918@users.noreply.github.com>
2021-07-05 17:41:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,8 +7,8 @@ const BLOG = {
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
+  lightBackground: '#FFF7EA', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#3A384A', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 20,
